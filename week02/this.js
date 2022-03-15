9d59ead5d125b274227998ff1c369c5eb7968106
EKKU LEIVONEN <ekku.leivonen@gmail.com>
2022-03-15 09:25:11 +0100

    Upload this. exec. 1-2

--- week02/this.js
+++ week02/this.js
@@ -44,24 +44,39 @@ Characters that are not alphabetic should not change. The toUpperCase and toLowe
 */
 
 function invertCase(str) {
+    var inverted = "";
     for (var i = 0; i < str.length; i++) {
         if (str[i] == str[i].toUpperCase()) {
-            console.log(str[i].toLowerCase());
+            inverted += str[i].toLowerCase();
         } else if (str[i] == str[i].toLowerCase()) {
-            console.log(str[i].toUpperCase());
+            inverted += str[i].toUpperCase();
+        } else {
+            inverted += str[i];
         }
     }
+    return inverted;
 }
 
-invertCase("AbCdEfG");
-
-//apply toUpperCase and toLowerCase to individual letters (you need to deconstruct thre string to an array?)
-//srt can be read like an array (you can use for loops for srtings --> srt[1] gives you second letter)
+console.log(invertCase("AbCdEfG000"));
 
 /*
 EXERCISE: BONUS
- 
+Write a constructor called Countdown that accepts a single argument - the number of seconds to count down.
+It should be possible to call the start method of instances of Countdown to initiate the countdown.
+Once the countdown starts, it should count down to zero starting with the number that was passed to the constructor and logging each number to the console with a one second delay.
 */
+
+function Countdown(seconds) {
+    this.seconds = seconds;
+    this.start = function () {
+        for (var i = seconds; i > 0; i--) {
+            console.log(i);
+        }
+    };
+}
+
+var countdown = new Countdown(5);
+countdown.start();
 
 //fix context issue with "this" keyword that occurs with the nested function
 //build start method with prototype?
