7ff8ddde3731eab5c6a7cb7e9f5faf32804bc4c2
EKKU LEIVONEN <ekku.leivonen@gmail.com>
2022-03-18 13:31:40 +0100

    PLAYGROUND: imageCovert live srver update

--- playground/imageConvert/script.js
+++ playground/imageConvert/script.js
@@ -5,11 +5,14 @@
     var canvasWidth = canvas.width;
     var canvasHeight = canvas.height;
     //create position memory
-    var positionX = 0;
-    var positionY = 0;
+    var positionX = 200;
+    var positionY = 200;
+    //grab image
+    var img = document.getElementById("cat400");
     //fill canvas with content (now a rectancle, later image or video...)
     ctx.fillStyle = "#f4c0de";
     ctx.fillRect(0, 0, 400, 400);
+    ctx.drawImage(img, 0, 0);
     //set sample Area size
     var sampleWidth = canvasWidth / 48;
     var sampleHeight = canvasHeight / 48;
@@ -22,26 +25,25 @@
     );
     //turn object into an array
     var selectedPixelData = selectedPixelDataObj.data;
-    console.log("Pixel data for area :", selectedPixelData);
 
     //Calculate avg brightness for given area on canvas
+
     areaBrightness();
     function areaBrightness() {
-        var areaSum = 0;
-        //Loop through each pixel (every 4th value in the array)
+        var areaSum = 0; //accumulate brightness of each pixel
+        //Loop through each pixel in selection (every 4th value in the array)
         for (var i = 0; i < selectedPixelData.length; i += 4) {
-            pxBrightness(selectedPixelData); //Calculate pixel brightness
-            function pxBrightness(selectedPixelData) {
-                for (var i = 0; i < selectedPixelData.length; i += 4) {
-                    var singlePx = selectedPixelData.slice(i, i + 4); //Separate each pixel from array
-                    var pxAvg = (singlePx[0] + singlePx[1] + singlePx[2]) / 3; //Average the RGB values of pixel
-                    console.log("Brightness value of pixel: ", pxAvg);
-                    areaSum = areaSum + pxAvg;
-                    return areaSum;
-                }
+            var singlePx = selectedPixelData.slice(i, i + 4); //Separate each pixel from array
+            pxBrightness(singlePx); //Calculate pixel brightness for the separated pixel
+            function pxBrightness(singlePx) {
+                var pxAvg = (singlePx[0] + singlePx[1] + singlePx[2]) / 3; //Average the RGB values of pixel
+                areaSum = areaSum += pxAvg; //add pixel brightness to accumulation
+                return pxAvg;
             }
         }
-        var areaAvg = areaSum / (selectedPixelData.length / 4);
+        var areaAvg = areaSum / (selectedPixelData.length / 4); //average area brightness
         console.log("Brightness value of area: ", areaAvg);
     }
+
+    // Create all needed areas to cover whole canvas
 })();
