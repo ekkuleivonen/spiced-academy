663032720963167e521ecba3e1b4001095ea3e0e
EKKU LEIVONEN <ekku.leivonen@gmail.com>
2022-03-26 12:45:31 +0100

    connect four v2

--- connectFour/script.js
+++ connectFour/script.js
@@ -1,65 +1,66 @@
 //currentPlayers
-var white = document.getElementById("white");
-var black = document.getElementById("black");
-var currentPlayer;
+var player1 = document.getElementById("player1");
+var player2 = document.getElementById("player2");
+var currentPlayer = "Player 1";
+var canPlay = true;
+var isActive = false;
 
 //store coins
 var coins = [];
-var coinsWhite = [];
-var coinsBlack = [];
 
 //Constructor for a new coin object
-function Coin(column, row, currentPlayer) {
+function Coin(column, row, currentPlayer, div) {
     this.column = column;
     this.row = row;
     this.currentPlayer = currentPlayer;
+    this.div = div;
 }
 
 //Coin prototype has another method that creates the real div on call
 Coin.prototype.createDiv = function (coinBelongsTo, currentPlayer) {
-    if (currentPlayer == "white") {
-        //style and place final white div
-        whiteFinal.classList.add("finalWhite");
-        coinBelongsTo.appendChild(whiteFinal);
-        whiteTemp.remove();
-        white.classList.remove("active");
-        black.classList.add("active");
-        currentPlayer = "black";
-    } else if (currentPlayer == "black") {
-        //style and place final black div
-        blackFinal.classList.add("finalBlack");
-        coinBelongsTo.appendChild(blackFinal);
-        blackTemp.remove();
-        black.classList.remove("active");
-        white.classList.add("active");
-        currentPlayer = "white";
+    if (currentPlayer == "Player 1") {
+        //style and place Mid player1 div
+        player1Mid.classList.add("midPlayer1");
+        coinBelongsTo.appendChild(player1Mid);
+        player1Pre.remove();
+        return player1Mid;
+    } else if (currentPlayer == "Player 2") {
+        //style and place Mid player2 div
+        player2Mid.classList.add("midPlayer2");
+        coinBelongsTo.appendChild(player2Mid);
+        player2Pre.remove();
+        return player2Mid;
     }
 };
 
-//Click on currentPlayer creates a temporary coin div
-white.addEventListener("click", createTempDiv);
-black.addEventListener("click", createTempDiv);
-var whiteTemp = document.createElement("div");
-var blackTemp = document.createElement("div");
-function createTempDiv(e) {
-    if (e.target.classList.contains("white")) {
-        //style and place temp white div
-        whiteTemp.classList.add("tempWhite");
-        document.body.appendChild(whiteTemp);
-        currentPlayer = "white";
-        //toggle player indicators
-        white.classList.add("active");
-        black.classList.remove("active");
-        blackTemp.remove();
-    } else if (e.target.classList.contains("black")) {
-        //style and place temp black div
-        blackTemp.classList.add("tempBlack");
-        document.body.appendChild(blackTemp);
-        currentPlayer = "black";
-        //toggle player indicators
-        black.classList.add("active");
-        white.classList.remove("active");
-        whiteTemp.remove();
+//Click on currentPlayer creates a Preorary coin div
+player1.addEventListener("click", createPreDiv);
+player2.addEventListener("click", createPreDiv);
+var player1Pre = document.createElement("div");
+var player2Pre = document.createElement("div");
+function createPreDiv(e) {
+    if (!canPlay) return;
+    isActive = true;
+    if (
+        e.target.classList.contains("player1coin") &&
+        currentPlayer == "Player 1"
+    ) {
+        //style and place Pre player1 div
+        player1Pre.classList.add("prePlayer1");
+        document.body.appendChild(player1Pre);
+        currentPlayer = "Player 1";
+        //remove player1 Pre
+        player2Pre.remove();
+    } else if (
+        e.target.classList.contains("player2coin") &&
+        currentPlayer == "Player 2"
+    ) {
+        //style and place Pre player2 div
+        player2Pre.classList.add("prePlayer2");
+        document.body.appendChild(player2Pre);
+        currentPlayer = "Player 2";
+        //remove player2 Pre
+        player1Pre.remove();
     }
 }
 
@@ -81,28 +82,42 @@ function findCoinColumn(e) {
     return coinBelongsTo, columnIndex;
 }
 
-//Space deletes tempDiv and places realDiv in the correct column
-var whiteFinal;
-var blackFinal;
+//Space deletes PreDiv and places realDiv in the correct column
+var player1Mid;
+var player2Mid;
 document.addEventListener("keydown", placeCoin);
 function placeCoin(e) {
-    whiteFinal = document.createElement("div");
-    blackFinal = document.createElement("div");
+    if (!isActive) return;
+    if (!canPlay) return;
+    if (coinBelongsTo.childElementCount > 5) return;
+    player1Mid = document.createElement("div");
+    player2Mid = document.createElement("div");
     if (e.keyCode == 32) {
         coins.push(
             new Coin(
                 columnIndex,
                 coinBelongsTo.childElementCount,
-                currentPlayer
+                currentPlayer,
+                Coin.prototype.createDiv(coinBelongsTo, currentPlayer)
             )
         );
-        Coin.prototype.createDiv(coinBelongsTo, currentPlayer);
+        isActive = false;
+        if (currentPlayer == "Player 1") {
+            player2.classList.remove("notActive");
+            player1.classList.add("notActive");
+        } else if (currentPlayer == "Player 2") {
+            player1.classList.remove("notActive");
+            player2.classList.add("notActive");
+        }
         checkRow(coinBelongsTo.childElementCount - 1);
         checkColumn(columnIndex);
         checkDiagonal(columnIndex, coinBelongsTo.childElementCount - 1);
-        ////run through column one position at a time
-
-        //currentPlayer = null;
+        //set next player
+        if (currentPlayer == "Player 1") {
+            return (currentPlayer = "Player 2");
+        } else if (currentPlayer == "Player 2") {
+            return (currentPlayer = "Player 1");
+        }
     }
 }
 
@@ -114,14 +129,15 @@ function checkRow(row) {
     var rowArr = coins.filter((Coin) => {
         return Coin.row === row;
     });
+    rowArr.sort((a, b) => {
+        return a.column - b.column;
+    });
     var count = 0;
     for (var r = 0; r < rowArr.length; r++) {
         if (rowArr[r].currentPlayer == currentPlayer) {
             count++;
             if (count == 4) {
-                console.log(currentPlayer, " wins");
-                victory(currentPlayer);
-                console.log(rowArr);
+                victory(currentPlayer, rowArr);
             }
         } else count = 0;
     }
@@ -132,14 +148,13 @@ function checkColumn(column) {
     var columnArr = coins.filter((Coin) => {
         return Coin.column === column;
     });
+
     var count = 0;
     for (var c = 0; c < columnArr.length; c++) {
         if (columnArr[c].currentPlayer == currentPlayer) {
             count++;
             if (count == 4) {
-                console.log(currentPlayer, " wins");
-                victory(currentPlayer);
-                console.log(columnArr);
+                victory(currentPlayer, columnArr);
             }
         } else count = 0;
     }
@@ -153,22 +168,19 @@ function checkDiagonal(colIdx, rowIdx) {
     // 3. create two empty arrays diag1 and diag2
     var diag1 = [];
     var diag2 = [];
-    // 4. Create a loop through all our 42 slots
-    // - for each slot calculate the rowIdx (slot.index()) and the colIdx (slot.parent().index()) of that slot
-    // IF the sum of that slot matches the sum from step 1 -> push slot to array diag1
+    // If sum of row idx and colmun idx match anything on diagcheck1 --> push
     for (var i = 0; i < coins.length; i++) {
         if (coins[i].row + coins[i].column == diagCheck1) {
             diag1.push(coins[i]);
         }
     }
-    // IF the diff of that slot matches the diff from step 2 -> push slot to array diag2
+    // If difference of row idx and colun idx match anything on diagcheck 2 --> push
     for (var j = 0; j < coins.length; j++) {
         if (coins[j].row - coins[j].column == diagCheck2) {
             diag2.push(coins[j]);
         }
     }
-    // 👆 we just created 2 arrays with the slots for the 2 possible winning diagonals
-    // now we want to pass those two array to the already existing checkForVictory.
+
     checkDiag1(diag1);
     function checkDiag1(diag1) {
         var count = 0;
@@ -176,9 +188,7 @@ function checkDiagonal(colIdx, rowIdx) {
             if (diag1[d1].currentPlayer == currentPlayer) {
                 count++;
                 if (count == 4) {
-                    console.log(currentPlayer, " wins");
-                    victory(currentPlayer);
-                    console.log(diag1);
+                    victory(currentPlayer, diag1);
                 }
             } else count = 0;
         }
@@ -190,56 +200,68 @@ function checkDiagonal(colIdx, rowIdx) {
             if (diag2[d2].currentPlayer == currentPlayer) {
                 count++;
                 if (count == 4) {
-                    console.log(currentPlayer, " wins");
-                    victory(currentPlayer);
-                    console.log(diag2);
+                    victory(currentPlayer, diag2);
                 }
             } else count = 0;
         }
     }
-    // 5. call checkVictory two times with diag1 and diag2 respectively
-    // - IF *any* of the checkForVictory calls return true -> return true
-    // - IF *none* of the checkForVictory callsr eturn true -> return false
 }
-
-function victory(currentPlayer) {
-    //do something
-    //setTimeout(resetGame, 3000);
+var h1 = document.querySelector("h1");
+var helperText = document.querySelector(".instructions p");
+function victory(currentPlayer, winningLine) {
+    canPlay = false;
+    for (var b = 0; b < winningLine.length; b++) {
+        if (winningLine[b].currentPlayer == currentPlayer) {
+            winningLine[b].div.classList.add("victory");
+        }
+    }
+    player1.classList.add("notActive");
+    player2.classList.add("notActive");
+    btn.innerText = "Play again";
+    h1.innerText = currentPlayer + " Wins!";
+    helperText.innerText =
+        'Restart the game by pressing "Play again" on the bottom right corner.';
 }
 
 //reset the game
 var btn = document.getElementById("btn");
 btn.addEventListener("click", resetGame);
 function resetGame() {
-    //delete white coins
-    var whiteCoins = document.querySelectorAll(".finalWhite");
-    for (var i = 0; i < whiteCoins.length; i++) {
-        whiteCoins[i].remove();
+    canPlay = true;
+    btn.innerText = "Reset";
+    h1.innerText = "Connect Four";
+    helperText.innerText =
+        "Click on player to start, use your mouse to find a position for the coin and release it with the space key.";
+    //delete player1 coins
+    var player1Coins = document.querySelectorAll(".midPlayer1");
+    for (var i = 0; i < player1Coins.length; i++) {
+        player1Coins[i].remove();
     }
-    //delete black coins
-    var blackCoins = document.querySelectorAll(".finalBlack");
-    for (var j = 0; j < blackCoins.length; j++) {
-        blackCoins[j].remove();
+    //delete player2 coins
+    var player2Coins = document.querySelectorAll(".midPlayer2");
+    for (var j = 0; j < player2Coins.length; j++) {
+        player2Coins[j].remove();
     }
     // empty coins array
     coins = [];
-    //remove active class from currentPlayers
-    black.classList.remove("active");
-    white.classList.remove("active");
-    //remove  temp divs
-    whiteTemp.remove();
-    blackTemp.remove();
+    //set player status back to normal
+    player1.classList.remove("notActive");
+    player2.classList.add("notActive");
+    currentPlayer = "Player 1";
+    //remove  Pre divs
+    player1Pre.remove();
+    player2Pre.remove();
 }
 
-//move temp divs with mouse
+//move Pre divs with mouse
 var mouseX;
 var mouseY;
 document.addEventListener("mousemove", trackMouse);
 function trackMouse(e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
-    whiteTemp.style.left = mouseX + "px";
-    whiteTemp.style.top = mouseY + "px";
-    blackTemp.style.left = mouseX + "px";
-    blackTemp.style.top = mouseY + "px";
+    player1Pre.style.left = mouseX + "px";
+    player1Pre.style.top = mouseY + "px";
+    player2Pre.style.left = mouseX + "px";
+    player2Pre.style.top = mouseY + "px";
 }
