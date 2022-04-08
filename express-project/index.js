77e543de9ff251a44116035c863afdd9b6f01515
EKKU LEIVONEN <ekku.leivonen@gmail.com>
2022-04-08 09:43:45 +0200

    basic auth for project spotify

--- express-project/index.js
+++ express-project/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const basicAuth = require("basic-auth");
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
@@ -19,7 +20,7 @@ app.post("/cookies.html", (req, res) => {
     res.redirect(req.cookies.reqURL);
 });
 
-//DEFINE ACCESS TO PAGES
+//DEFINE COOKIE ACCESS TO PAGES
 app.use((req, res, next) => {
     if (req.cookies.consent != "true" && !req.url.includes("cookies.html")) {
         //Store previous URL in a cookie
@@ -27,11 +28,30 @@ app.use((req, res, next) => {
         res.redirect("/cookies.html");
     } else next();
 });
-
+//SERVE INDEX.HTML
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+//REQUIRE PASSWORD FOR SPOTIFY SEARCH
+//DEFINE BASIC AUTH
+const auth = function (req, res, next) {
+    const creds = basicAuth(req);
+    if (!req.url.includes("SpotifySearch")) return next();
+    if (!creds || creds.name != "testuser" || creds.pass != "testpassword") {
+        console.log("SHOW ME YOURSELF");
+        res.setHeader(
+            "WWW-Authenticate",
+            'Basic realm="Enter your credentials to see this stuff."'
+        );
+        res.sendStatus(401);
+    } else {
+        next();
+    }
+};
+app.use(auth);
+
+//SERVE PROJECTS
 app.use(express.static(path.join(__dirname, "projects")));
 
 app.listen(3000, () => console.log("listening on port 3000..."));
