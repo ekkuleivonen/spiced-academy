e800035248b6bfdded06a5ff9100a995f2059be9
EKKU LEIVONEN <ekku.leivonen@gmail.com>
2022-04-13 16:57:14 +0200

    forgotten catch

--- week06/fun-with-fs-promises/index.js
+++ week06/fun-with-fs-promises/index.js
@@ -6,27 +6,31 @@ const inputPath = "/Users/ekkuleivonen/aspartame-code/week05/fun-with-fs/files";
 logSizes(inputPath);
 
 function logSizes(inputPath) {
-    readdir(inputPath, { withFileTypes: true }).then((files) => {
-        files.forEach((file) => {
-            stat(`${inputPath}/${file.name}`)
-                .then((itemData) => {
-                    if (file.name.includes(".DS_Store")) return;
-                    if (itemData.isFile()) {
-                        console.log(
-                            inputPath +
-                                "/" +
-                                file.name +
-                                ": " +
-                                itemData.size +
-                                " bytes"
-                        );
-                    } else {
-                        logSizes(`${inputPath}/${file.name}`);
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+    readdir(inputPath, { withFileTypes: true })
+        .then((files) => {
+            files.forEach((file) => {
+                stat(`${inputPath}/${file.name}`)
+                    .then((itemData) => {
+                        if (file.name.includes(".DS_Store")) return;
+                        if (itemData.isFile()) {
+                            console.log(
+                                inputPath +
+                                    "/" +
+                                    file.name +
+                                    ": " +
+                                    itemData.size +
+                                    " bytes"
+                            );
+                        } else {
+                            logSizes(`${inputPath}/${file.name}`);
+                        }
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            });
+        })
+        .catch((error) => {
+            console.log(error);
         });
-    });
 }
