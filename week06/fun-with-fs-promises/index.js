a1b2b99490b57773d6dc2c6020a902db9b39fd64
EKKU LEIVONEN <ekku.leivonen@gmail.com>
2022-04-13 16:54:44 +0200

    fs_promises

---
+++ week06/fun-with-fs-promises/index.js
@@ -0,0 +1,32 @@
+const { readdir, stat } = require("fs").promises;
+
+const inputPath = "/Users/ekkuleivonen/aspartame-code/week05/fun-with-fs/files";
+
+//Part1
+logSizes(inputPath);
+
+function logSizes(inputPath) {
+    readdir(inputPath, { withFileTypes: true }).then((files) => {
+        files.forEach((file) => {
+            stat(`${inputPath}/${file.name}`)
+                .then((itemData) => {
+                    if (file.name.includes(".DS_Store")) return;
+                    if (itemData.isFile()) {
+                        console.log(
+                            inputPath +
+                                "/" +
+                                file.name +
+                                ": " +
+                                itemData.size +
+                                " bytes"
+                        );
+                    } else {
+                        logSizes(`${inputPath}/${file.name}`);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        });
+    });
+}
